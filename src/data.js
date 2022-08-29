e8a4806651a2251929f47a828b9ccf5157d6109d
Muhammad Alyan <alyan0332@gmail.com>
2022-08-29 21:25:12 +0500

    testimonials and reviews data

---
+++ src/data.js
@@ -0,0 +1,39 @@
+import coltonAvatar from './assets/images/image-colton.jpg';
+import anneAvatar from './assets/images/image-anne.jpg';
+import ireneAvatar from './assets/images/image-irene.jpg';
+
+export const testimonials = [
+  {
+    img: coltonAvatar,
+    name: 'Colton Smith',
+    verifiedBuyer: true,
+    review:
+      'We needed the same printed design as the one we had ordered a week prior. Not only did they find the original order, but we also received it in time. Excellent!',
+  },
+  {
+    img: anneAvatar,
+    name: 'Irene Roberts',
+    verifiedBuyer: true,
+    review:
+      'Customer service is always excellent and very quick turn around. Completely delighted with the simplicity of the purchase and the speed of delivery.',
+  },
+  {
+    img: ireneAvatar,
+    name: 'Anne Wallace',
+    verifiedBuyer: true,
+    review:
+      'Put an order with this company and can only praise them for the very high standard. Will definitely use them again and recommend them to everyone!',
+  },
+];
+
+export const fiveStarReviews = [
+  {
+    name: 'Reviews',
+  },
+  {
+    name: 'Report Guru',
+  },
+  {
+    name: 'BestTech',
+  },
+];
