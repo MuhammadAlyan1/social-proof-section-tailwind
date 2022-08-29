87c95a395b5d52f0e7d27a9d9b01ad0239e6c4ad
Muhammad Alyan <alyan0332@gmail.com>
2022-08-29 21:26:59 +0500

    create seperate component for heading

---
+++ src/components/reviews/Heading.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Heading = () => {
+  return (
+    <div>
+      <h1 className="text-center text-4xl font-bold text-fuchsia-900 lg:w-[430px] lg:text-left lg:text-5xl">
+        10,000+ of our users love our products.
+      </h1>
+      <p className="my-6 text-center text-lg font-medium text-slate-700 lg:w-[430px] lg:text-left ">
+        We only provide great products combined with excellent customer service.
+        See what our satisfied customers are saying about our services.
+      </p>
+    </div>
+  );
+};
+
+export default Heading;
